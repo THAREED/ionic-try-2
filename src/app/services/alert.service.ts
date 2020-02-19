bceb3218c1ad506d1f0314bd09b6e5b48cb4576e
veriest <kankawin@hot-now.com>
2020-02-20 02:42:14 +0700

    Update work flow

--- src/app/services/alert.service.ts
+++ src/app/services/alert.service.ts
@@ -8,13 +8,4 @@ export class AlertService {
 
   constructor(private toastController: ToastController) { }
 
-  async presentToast(message: any) {
-    const toast = await this.toastController.create({
-      message: message,
-      duration: 2000,
-      position: 'top',
-      color: 'dark'
-    });
-    toast.present();
-  }
 }

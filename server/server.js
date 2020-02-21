5a605b5e7cfda0a0e65a8b26f83452f8145be42b
veriest <kankawin@hot-now.com>
2020-02-21 11:43:07 +0700

    Update docker-compose

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(
 
 app.get('/', (request, response) => {
     response.json({
-        info: 'Node.js, Express, and Postgres API Yeah'
+        info: 'Node.js, Express, and Postgres API'
     })
 })
 

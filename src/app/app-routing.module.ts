0fa1c71a6b3b89a83bc9e6dafbde1b455b09daa8
veriest <kankawin@hot-now.com>
2020-02-21 12:00:44 +0700

    Update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,21 +3,11 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './guard/auth.guard';
 
 const routes: Routes = [
-  // { path: '', loadChildren: './pages/auth/login/login.module#LoginPageModule' },
-  // { path: 'login', loadChildren: './pages/auth/login/login.module#LoginPageModule' },
-  // { path: 'register', loadChildren: './pages/auth/register/register.module#RegisterPageModule' },
-  // // Redirect to tab-routing.module
-  // { path: 'app', loadChildren: './tabs/tabs.module#TabsPageModule', canActivate: [AuthGuard]},
-  // { path: 'lesson', loadChildren: './pages/lessons/lessons.module#LessonsPageModule' },
-  // { path: 'exercise', loadChildren: './pages/exercises/exercises.module#ExercisesPageModule' },
-  // { path: 'exam', loadChildren: './pages/exams/exams.module#ExamsPageModule' },
-  // { path: 'screening', loadChildren: './pages/screening/screening.module#ScreeningPageModule' },
-
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule'},
+  { path: '', loadChildren: './pages/auth/login/login.module#LoginPageModule' },
   { path: 'login', loadChildren: './pages/auth/login/login.module#LoginPageModule' },
   { path: 'register', loadChildren: './pages/auth/register/register.module#RegisterPageModule' },
   // Redirect to tab-routing.module
-  { path: 'app', loadChildren: './tabs/tabs.module#TabsPageModule'},
+  { path: 'app', loadChildren: './tabs/tabs.module#TabsPageModule', canActivate: [AuthGuard]},
   { path: 'lesson', loadChildren: './pages/lessons/lessons.module#LessonsPageModule' },
   { path: 'exercise', loadChildren: './pages/exercises/exercises.module#ExercisesPageModule' },
   { path: 'exam', loadChildren: './pages/exams/exams.module#ExamsPageModule' },
